b6123a697ba0f1596469cabcfb58589f60ba2f43
Sjouke91 <s.bosma1991@gmail.com>
2020-11-24 14:06:18 +0100

    Added .env for hiding elephantSQL url

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config(); // we configure dotenv
+module.exports = {
+  development: {
+    url: process.env.ELEPHANT_SQL, // and use the env vars to get the url
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: "0",
+  },
+};
